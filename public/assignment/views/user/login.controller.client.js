4ad4052af1df7d523d1310348a0d953c6b6e4e4a
maxrais <max.j.rais@gmail.com>
2016-05-25 14:20:41 -0400

    refactored controllers and added service

---
+++ public/assignment/views/user/login.controller.client.js
@@ -0,0 +1,26 @@
+/**
+ * Created by MaxRais on 5/24/16.
+ */
+
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController", LoginController);
+
+    function LoginController($location, UserService) {
+
+        var vm = this;
+        vm.login = login;
+
+        function login (username, password) {
+            var user = UserService.findUserByUsernameAndPassword(username, password);
+            if(user) {
+                var id = user._id;
+                $location.url("/profile/"+id);
+            }
+            else {
+                vm.error = "User not found";
+            }
+        }
+    }
+})();
---
+++ public/assignment/views/user/profile.controller.client.js
@@ -0,0 +1,36 @@
+/**
+ * Created by MaxRais on 5/24/16.
+ */
+
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("ProfileController", ProfileController);
+
+    function ProfileController($routeParams, UserService) {
+        var vm = this;
+        vm.updateUser = updateUser;
+        var id = $routeParams["id"];
+
+        init();
+
+        function updateUser() {
+            var result = UserService.updateUser(vm.user._id, vm.user);
+            if(result) {
+                vm.success = "User successfully updated";
+            }
+            else {
+                vm.error = "Could not update user";
+            }
+
+        }
+
+        function init() {
+            var user = UserService.findUserById(id);
+            if(user) {
+                vm.user = angular.copy(user);
+            }
+        }
+    }
+    
+})();
