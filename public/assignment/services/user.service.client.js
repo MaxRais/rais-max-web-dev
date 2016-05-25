2d688c07a748cc7d9824ed9ac16e6ce1f82ac104
maxrais <max.j.rais@gmail.com>
2016-05-25 14:31:28 -0400

    added navigation to website-list

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -15,6 +15,15 @@
             {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
         ];
 
+        var websites = [
+            { "_id": "123", "name": "Facebook",    "developerId": "456" },
+            { "_id": "234", "name": "Tweeter",     "developerId": "456" },
+            { "_id": "456", "name": "Gizmodo",     "developerId": "456" },
+            { "_id": "567", "name": "Tic Tac Toe", "developerId": "123" },
+            { "_id": "678", "name": "Checkers",    "developerId": "123" },
+            { "_id": "789", "name": "Chess",       "developerId": "234" }
+        ]
+
         var api = {
           createUser: createUser,
           findUserByUsernameAndPassword: findUserByUsernameAndPassword,
