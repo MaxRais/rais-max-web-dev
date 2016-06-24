82eabb79d4fc923d1f682386381a10c5d45d9d87
maxrais <max.j.rais@gmail.com>
2016-06-24 16:24:45 -0400

    starting following page

---
+++ public/project/views/user/following.controller.client.js
@@ -0,0 +1,39 @@
+/**
+ * Created by MaxRais on 6/24/16.
+ */
+
+(function () {
+    angular
+        .module("ChallongeClient")
+        .controller("FollowingController", FollowingController);
+
+    function FollowingController($location, $window, $routeParams, $rootScope, UserService, ChallongeService) {
+        var vm = this;
+
+        function init() {
+            vm.user = JSON.parse($window.localStorage.getItem("currentUser"));
+            vm.following = [vm.user];
+
+            for(var index in vm.user.following) {
+                var id = vm.user.following[index];
+                UserService
+                    .findUserById(id)
+                    .then(function(user) {
+                        vm.following.push(user);
+                        var bracketIds = user.participating.map(function(p) {
+                            return p.bracketId;
+                        });
+                        for(var b in bracketIds) {
+                            ChallongeService
+                                .findOneTournament(bracketIds[b])
+                                .then(function(res) {
+                                    vm.following[index].pTournaments.push(res.data.tournament);
+                                });
+                        }
+                    });
+            }
+        }
+
+        init();
+    }
+})();
