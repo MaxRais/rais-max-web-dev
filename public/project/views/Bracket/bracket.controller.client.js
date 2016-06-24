090a2bdbea7fcfedb990b2920af44183d556d796
kaltschu <kevinaltschuler@gmail.com>
2016-06-23 21:49:56 -0400

    lots

--- public/project/views/Bracket/bracket.controller.client.js
+++ public/project/views/Bracket/bracket.controller.client.js
@@ -7,21 +7,24 @@
         .module("ChallongeClient")
         .controller("BracketController", BracketController);
 
-    function BracketController($location, $window, $rootScope, $routeParams, ChallongeService) {
+    function BracketController($location, $scope, $window, $rootScope, $routeParams, ChallongeService) {
 
         var vm = this;
+        vm.submitMatch= submitMatch;
+        vm.showModal = showModal;
         function init() {
             vm.user = JSON.parse($window.localStorage.getItem("currentUser"));
             vm.activeMatches = [];
             vm.pendingMatches = [];
             vm.completedMatches = [];
+            vm.activeMatch = {player1: 'asd', player2: 'asdasd'};
             ChallongeService
                 .getOneTournament($routeParams.url)
                 .then(
                     function(res) {
                         vm.bracket = res.data.tournament;
                         ChallongeService
-                            .getMatches("cs3200")
+                            .getMatches(vm.bracket.url)
                             .then(
                                 function(res) {
                                     vm.matches = res.data;
@@ -35,10 +38,6 @@
                                         else if(match.state == "complete")
                                             vm.completedMatches.push(match);
                                     }
-
-                                    console.log(vm.activeMatches);
-                                    console.log(vm.pendingMatches);
-                                    console.log(vm.completedMatches);
                                     
                                 }
                             ); 
@@ -49,6 +48,42 @@
                 );
         }
         init();
+
+        function showModal(match) {
+            vm.activeMatch = match;
+            $scope.p1Score = null;
+            $scope.p2Score = null;
+            $scope.winner = null;
+            $('#matchModal').modal('toggle');
+        }
+
+        function myscope($scope) {
+          $scope.button = 'red';
+        }
+
+        function submitMatch() {
+            var winner;
+            if($scope.winner == 1) 
+                winner = vm.activeMatch.player1_id
+            if($scope.winner == 2)
+                winner = vm.activeMatch.player2_id;
+            if(winner)
+                ChallongeService
+                    .updateMatch(vm.bracket.url, vm.activeMatch.id, $scope.p1Score, $scope.p2Score)
+                    .then(
+                        function(res) {
+                            console.log(res);
+                            for(var key in vm.activeMatches) {
+                                var match = vm.activeMatches[key];
+                                if(match.id == vm.activeMatch.id)
+                                    vm.activeMatches[key] = res.data;
+                            }
+                        },
+                        function(err) {
+                            console.log(err);
+                        }
+                    )
+        }
 
     }
 })();
